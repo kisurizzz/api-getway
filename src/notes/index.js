ed473cbba61a11c8ca15a3565df676265626ac2a
Arnold Kisuri <arnoldkisuri7@gmail.com>
2025-05-13 08:18:21 +0300

    Update CORS settings in notes and todos Lambda functions to allow all origins; add Jest as a testing framework in package.json for both services.

--- src/notes/index.js
+++ src/notes/index.js
@@ -29,7 +29,7 @@ const authenticateRequest = (event) => {
 // Helper function for CORS headers
 const corsHeaders = {
   "Content-Type": "application/json",
-  "Access-Control-Allow-Origin": "https://api-getway-nine.vercel.app",
+  "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Headers":
     "Content-Type,Authorization,X-Amz-Date,X-Api-Key,X-Amz-Security-Token",
   "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
