4077fa4029484c0abf6be1034639f819c36dc7cb
Arnold Kisuri <arnoldkisuri7@gmail.com>
2025-06-16 20:57:23 +0300

    Refactor getNotes and getTodos functions to use query instead of scan for improved performance; update DynamoDB parameters to include IndexName and KeyConditionExpression for efficient data retrieval.

--- src/notes/index.js
+++ src/notes/index.js
@@ -121,13 +121,14 @@ exports.handler = async (event) => {
 async function getNotes(user) {
   const params = {
     TableName: TABLE_NAME,
-    FilterExpression: "userId = :userId",
+    IndexName: "UserIdIndex",
+    KeyConditionExpression: "userId = :userId",
     ExpressionAttributeValues: {
       ":userId": user.userId,
     },
   };
 
-  const result = await dynamoDB.scan(params).promise();
+  const result = await dynamoDB.query(params).promise();
 
   return {
     statusCode: 200,
